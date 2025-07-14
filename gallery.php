ec6eefb08930998a1b03abf33bc7d9d6e4b567a5
Imran <syed16imran@gmail.com>
2025-07-14 22:19:35 +0330

    logo font awesome removed

--- gallery.php
+++ gallery.php
@@ -8,6 +8,27 @@
   <meta name="keywords" content="gallery, photos">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
+
+  <!-- Favicon -->
+    <link href="img/favicon.ico" rel="icon">
+
+    <!-- Google Web Fonts -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link
+        href="https://fonts.googleapis.com/css2?family=Jost:wght@500;600;700&family=Open+Sans:wght@400;600&display=swap"
+        rel="stylesheet">
+
+    <!-- Font Awesome -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
+
+    <!-- Libraries Stylesheet -->
+    <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+
+    <!-- Customized Bootstrap Stylesheet -->
+    <link href="css/style.css" rel="stylesheet">
+
+
+
   <!-- Stylesheets -->
   <link rel="stylesheet" href="gallery/css/bootstrap.min.css" />
   <link rel="stylesheet" href="gallery/css/font-awesome.min.css" />
@@ -24,13 +45,28 @@
 
   <!-- Navbar -->
   <?php
-  include("navbar.php");
+  include "navbar.php";
   ?>
   <!-- Navbar End -->
 
   <!-- hero -->
 
-
+<div class="jumbotron jumbotron-fluid page-header position-relative overlay-bottom" style="margin-bottom: 90px;">
+        <div class="container text-center py-5">
+            <h1 class="text-white display-1">Gallery</h1>
+            <div class="d-inline-flex text-white mb-5">
+                <p class="m-0 text-uppercase"><a class="text-white" href="">Home</a></p>
+                <i class="fa fa-angle-double-right pt-1 px-3"></i>
+                <p class="m-0 text-uppercase">Gallery</p>
+            </div>
+            <div class="mx-auto mb-5" style="width: 100%; max-width: 600px;">
+                <div class="input-group">
+                    
+                   
+                </div>
+            </div>
+        </div>
+    </div>
 
   <!-- hero -->
 
@@ -69,6 +105,24 @@
   <script src="gallery/js/slick.min.js"></script>
   <script src="gallery/js/fresco.min.js"></script>
   <script src="gallery/js/main.js"></script>
+
+
+
+  <!-- Back to Top -->
+    <a href="#" class="btn btn-lg btn-primary rounded-0 btn-lg-square back-to-top"><i
+            class="fa fa-angle-double-up"></i></a>
+
+
+    <!-- JavaScript Libraries -->
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js"></script>
+    <script src="lib/easing/easing.min.js"></script>
+    <script src="lib/waypoints/waypoints.min.js"></script>
+    <script src="lib/counterup/counterup.min.js"></script>
+    <script src="lib/owlcarousel/owl.carousel.min.js"></script>
+
+    <!-- Template Javascript -->
+    <script src="js/main.js"></script>
 </body>
 
 </html>
